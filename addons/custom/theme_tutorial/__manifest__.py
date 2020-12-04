28bd504dcea048e438017dee97d47f8a8e1c2d5d
Juan Ramón <jrpicosu@gmail.com>
2020-12-04 12:51:10 +0100

    Creación de layout con código y modificación de data en manifest.

--- addons/custom/theme_tutorial/__manifest__.py
+++ addons/custom/theme_tutorial/__manifest__.py
@@ -1,11 +1,10 @@
 {
-  'name':'Tutorial theme',
-  'description': 'Tema del tutorial de Odoo.',
-  'version':'1.0',
-  'author':'Jotaerre',
+    'name': 'Tutorial theme',
+    'description': 'Tema del tutorial de Odoo.',
+    'version': '1.0',
+    'author': 'Jotaerre',
 
-  'data': [
-  ],
-  'category': 'Theme/Creative',
-  'depends': ['website', 'website_theme_install'],
+    'data': ['views/layout.xml'],
+    'category': 'Theme/Creative',
+    'depends': ['website', 'website_theme_install'],
 }
