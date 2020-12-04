b8b448ad39dbb31e24e640c9b16ccd6542813267
Juan Ramón <jrpicosu@gmail.com>
2020-12-04 12:47:21 +0100

    Adicción de código a manifest.

---
+++ addons/custom/theme_tutorial/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  'name':'Tutorial theme',
+  'description': 'Tema del tutorial de Odoo.',
+  'version':'1.0',
+  'author':'Jotaerre',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_theme_install'],
+}
